998f575c05ce83955e1500ccaff8238fec6224bc
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-09-20 09:52:27 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -30,7 +30,7 @@
     <body class="hold-transition login-page">
         <div class="login-box">
             <div class="login-logo">
-                <a href="<?php echo base_url(); ?>/adminlte/index2.html"><b>SIM <?php echo getInfoRS('nama_rumah_sakit')?></b></a>
+                <a href="<?php echo base_url(); ?>/adminlte/index2.html"><img src="<?= base_url()."assets/images/".getInfoRS('logo') ?>" alt="" width="50"> <b>SIM <?php echo getInfoRS('nama_rumah_sakit')?></b></a>
             </div>
             <!-- /.login-logo -->
             <div class="login-box-body">
