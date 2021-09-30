52453fcef807a8d9d47a08b8d238744407d34aa3
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-09-30 15:06:49 +0700

    Update cetak_struk.php

--- application/views/pembayaran/cetak_struk.php
+++ application/views/pembayaran/cetak_struk.php
@@ -1,189 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <title>Kwitansi Pembayaran</title>
-
-  <!-- Normalize or reset CSS with your favorite library -->
-  <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">-->
-
-  <!-- Load paper.css for happy printing -->
-  <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.3.0/paper.css">-->
-   <!-- <link rel="stylesheet" href="<?php echo base_url() ?>assets/paper-css/paper.css"> -->
-   <link rel="stylesheet" href="<?php echo base_url() ?>assets/paper-css/paper.css">
-
-   <style>
-	  body{
-      font-family : "Arial";
-	  }
-	  .header, .header .left{
-		  display: flex;
-	  }
-    .header .left{
-      width:55%;
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Cetak Struk</title>
+  <style type="text/css">
+  *{
+        /* font-family : "Dot Matrix"; */
+        font-size : 15px;
+        color: black;
     }
-	  .header .right{
-		  width: 45%;
-      padding-left:40px;
-      padding-top:30px;
-	  }
+  /* @media print and (width: 58mm) and (height: 80mm) { */
+        /* @page {
+          margin: 3cm;
+        } */
+  /* } */
+  /* @page {
+      size: 58mm 80mm;
+      margin: 10%;
+  } */
+  body{
+    width: 58mm;
+    min-height: 80mm;
+  }
+  /* @media print { 
+    body{ 
+      width: 58mm;
+      height: 80mm
+    }
+  } */
   </style>
-  <style>@page { size: A5 }</style>
-
-  <!-- Set page size here: A5, A4 or A3 -->
-  <!-- Set also "landscape" if you need -->
-  <!--<style>
-  @page { size: A5 landscape }
-  </style>-->
-
+  <link rel="shortcut icon" href="{{ asset('public/assets/img/icon2.png') }}" />
 </head>
-
-<!-- Set "A5", "A4" or "A3" for class name -->
-<!-- Set also "landscape" if you need -->
-<!-- <body class="A5 landscape" onload="window.print()"> -->
-<body class="A5 landscape">
-  <!-- Each sheet element should have the class "sheet" -->
-  <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
-  <section class="sheet padding-10mm">
-
-	<div class="header">
-		<div class="left">
-      <div class="img">
-        <img src="<?php echo base_url()."assets/images/".getInfoRS('logo')?>" alt="logo" width="70" />
-      </div>
-      <div class="address" style="margin-left:15px">
-          <h2 style="font-family:times-new-roman;margin-top:0;margin-bottom:0px"><?= getInfoRS('nama_rumah_sakit') ?></h2>
-          <p style="margin-top:5px;margin-bottom:0px"><?= getInfoRS('alamat') ?></p>
-          <p style="margin-top:5px;margin-bottom:0px"><?= getInfoRS('no_telpon') ?></p>
-      </div>
-		</div>
-	  <div class="right">
-      <table>
-        <tr>
-          <td>Number</td>
-          <td>:</td>
-          <td><u><?php echo $transaksi[0]->no_transaksi;?></u></td>
-        </tr>
-        <tr>
-          <td>Date</td>
-          <td>:</td>
-          <td><u><?= date('d-m-Y') ?></u></td>
-        </tr>
-      </table>
-		</div>
-	  <!--<h4 style="text-align: left;">Ruko Atrani 24 - Sukorahayu - Wagir - Telp. (0341) 806305</h4>-->
-	</div>
-<h3 style="text-align: center;margin-bottom:0px"><span style="text-decoration: underline;"><i>BILL</i></span></h3><p>
-<center>
-    <table width="90%">
-      <tr>
-        <td width="50%">
-          <h3 style="margin-bottom:5px"><u>IDENTITY</u></h3>
-          <table>
-            <tr>
-              <td>Name &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
-              <td>:</td>
-              <td><?= $transaksi[0]->atas_nama ?></td>
-            </tr>
-            <tr>
-              <td>Age</td>
-              <td>:</td>
-              <td>...............</td>
-            </tr>
-            <tr>
-              <td>Address</td>
-              <td>:</td>
-              <td>...............</td>
-            </tr>
-          </table>
-        </td>
-        <td width="5%"></td>
-        <td width="45%">
-        <table style="margin-top:30px">
-            <tr>
-              <td>Sex &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>
-              <td>:</td>
-              <td>...............</td>
-            </tr>
-            <tr>
-              <td>Nationality</td>
-              <td>:</td>
-              <td>...............</td>
-            </tr>
-            <tr>
-              <td>Room Number</td>
-              <td>:</td>
-              <td>...............</td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-    </table>
-    <center>
-  <table width="90%">
+<body>
+  <center>
+    <p style="margin-bottom:0px">Nada Farma</p>
+    <?php echo $transaksi[0]->no_transaksi;?>
+    <br>        
+    -------------------------------------------
+  </center>
+  <table width="100%">
     <tr>
-      <td width="45%">
-        <h3 style="margin-bottom:5px"><u>FOR PAYMENT</u></h3>
-          <table width="100%">
-            <?php
-            $total_transaksi = 0;
-            $i = 1;
-            foreach($transaksi as $data){
-              $total_transaksi+=($data->harga*$data->jumlah);
-                  ?>
-            <tr>
-            <td><?php echo $data->nama_barang."(".$data->jumlah.")";?></td>
-            <td>:</td>
-            <td>Rp.<?php echo number_format(($data->harga*$data->jumlah),2,',','.')?></td>
-            </tr>
-            <?php 
-            }
-            if ($getDiskon != null) {
-              $diskon=$total_transaksi*(($getDiskon != null ? $getDiskon->diskon : 0) / 100);
-            ?>
-            <tr>
-              <td>Subsidy</td>
-              <td>:</td>
-              <td>Rp.<?php echo number_format($diskon,2,',','.')?></td>
-            </tr>
-            <?php } else{ $diskon = 0; } ?>
-            <tr>
-              <td colspan="3"><hr></td>
-            </tr>
-            <tr>
-                <td width="85%"><b>Amount</b></td>
-                <td width="10%"><b>Rp</b></td>
-                <td width="35%"><b><?php echo number_format($total_transaksi-$diskon,2,',','.');?></b></td>
-            </tr>
-          </table>
+      <td width="47.5%">
+        <?= date('Y-m-d') ?>
+        <br>
+        <?= date('H:i:s') ?>
       </td>
-      <td width="10%"></td>
-      <td width="45%">
-        <table width="100%">
-          <tr>
-            <td><span style="margin-left:50px"></span> Denpasar, <?php echo date('d-m-Y');?></td>
-          </tr>
-        </table>
-      </td>
+      <td width="56%"></td>
+      <td width="47.5%" align="right"><?= $transaksi[0]->atas_nama ?></td>
     </tr>
   </table>
-  <br>
-</center>
-<div style="width:55%;margin-left:auto">
-  <span>(Guest Signature)</span>
-  <span style="margin-left:70px">(Attending Physician)</span>
-</div>
-</div>
-  </section>
+  -------------------------------------------
+  <table width="100%">
+    <?php
+      $total_transaksi = 0;
+      $i = 1;
+      foreach($transaksi as $data){
+        $total_transaksi+=($data->harga*$data->jumlah);
+    ?>
+    <tr>
+      <td width="47.5%">
+        <b><?= $data->nama_barang ?></b>
+        <br>
+        <?= $data->jumlah." x ".$data->harga ?>
+      </td>
+      <td width="5%"></td>
+      <td width="47.5%" align="right">
+        <br>
+        Rp.<?php echo number_format(($data->harga*$data->jumlah),2,',','.')?>
+      </td>
+    </tr>
+    <?php } 
+        $diskon=$total_transaksi*(($getDiskon != null ? $getDiskon->diskon : 0) / 100);
+    ?>
+    <tr>
+      <td colspan="3">------------------------------------------</td>
+    </tr>
+    <tr>
+      <td width="47.5%">
+        <b>Total</b>
+      </td>
+      <td width="5%"></td>
+      <td width="47.5%" align="right">
+        Rp. <?php echo number_format($total_transaksi,2,',','.');?>
+      </td>
+    </tr>    
+    <tr>
+      <td width="47.5%">
+        <b>Diskon</b>
+      </td>
+      <td width="5%"></td>
+      <td width="47.5%" align="right">
+        Rp. <?php echo number_format($diskon,2,',','.');?>
+      </td>
+    </tr>    
+    <tr>
+      <td width="47.5%">
+        <b>Grand Total</b>
+      </td>
+      <td width="5%"></td>
+      <td width="47.5%" align="right">
+        Rp. <?php echo number_format($total_transaksi-$diskon,2,',','.');?>
+      </td>
+    </tr>    
+  </table>
 
 </body>
-<script type="text/javascript">
-    window.print();
-    // window.onmousemove = function() {
-    //   window.location= '<?=base_url("apotek")?>'
-    // }  
-
+<script>
+ window.print();
 </script>
 </html>
 
