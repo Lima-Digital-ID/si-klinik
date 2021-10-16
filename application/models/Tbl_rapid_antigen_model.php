77957e38ae27a09a89b208f4daa8ae8e1f517505
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-10-16 10:28:55 +0700

    Update Tbl_rapid_antigen_model.php

--- application/models/Tbl_rapid_antigen_model.php
+++ application/models/Tbl_rapid_antigen_model.php
@@ -59,12 +59,12 @@ class Tbl_rapid_antigen_model extends CI_Model
     }    
     public function detailRapid($id,$select="")
     {
-        $s = $select!="" ? $select : /* "tbl_dokter.nama_dokter,". */$this->table.".*";
+        $s = $select!="" ? $select : "d.nama_dokter,r.*";
         $this->db->select($select);
-        $this->db->from($this->table);
-        // $this->db->join('tbl_dokter',$this->table.".id_dokter = tbl_dokter.id_dokter",'left');
-        $this->db->where(['id_rapid' => $id]);
-        return $this->db->get()->result()[0];
+        $this->db->from("tbl_rapid_antigen r");
+        $this->db->join('tbl_dokter d',"r.id_dokter = d.id_dokter",'left');
+        $this->db->where(['r.id_rapid' => $id]);
+        return $this->db->get()->row();
     }
     public function cekPeriksa($id)
     {
