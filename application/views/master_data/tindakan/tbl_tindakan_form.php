ef8ebab28ccb6869441fd8fc7b652822137f8ec0
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2021-10-26 20:05:58 +0700

    Update tbl_tindakan_form.php

--- application/views/master_data/tindakan/tbl_tindakan_form.php
+++ application/views/master_data/tindakan/tbl_tindakan_form.php
@@ -8,7 +8,7 @@
             <form action="<?php echo $action; ?>" method="post" enctype="multipart/form-data">
                 <table class='table table-bordered>'        
                         <tr><td width='200'>Kode Tindakan <?php echo form_error('kode_tindakan') ?></td><td><input type="text" class="form-control" name="kode_tindakan" id="kode_tindakan" placeholder="Kode Tindakan" value="<?php echo $kode_tindakan; ?>" 
-                        <?php if(isset($kode_tindakan)) echo 'readonly'; else ''; ?> /></td></tr>
+                        <?php if(isset($kode_tindakan) && $kode_tindakan!='') echo 'readonly'; else ''; ?> /></td></tr>
                         <tr><td width='200'>Tindakan <?php echo form_error('tindakan') ?></td><td><input type="text" class="form-control" name="tindakan" id="tindakan" placeholder="Tindakan" value="<?php echo $tindakan; ?>" /></td></tr>
                         <tr><td width='200'>Biaya <?php echo form_error('biaya') ?></td><td><input type="number" class="form-control" name="biaya" id="biaya" placeholder="Biaya" value="<?php echo $biaya; ?>" /></td></tr>
                         <tr><td width='200'>Tipe <?php echo form_error('tipe') ?></td><td>
