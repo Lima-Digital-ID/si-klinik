ac739596fdbc682b7acc5ca65f4108fdc09cd9c9
davidsetyaahr <44167895+davidsetyaahr@users.noreply.github.com>
2022-02-02 12:34:57 +0700

    Update report_profit_loss.php

--- application/views/akuntansi/laporan/report_profit_loss.php
+++ application/views/akuntansi/laporan/report_profit_loss.php
@@ -18,15 +18,15 @@
                                         <label>Pilih Bulan : </label>
                                         <select class="form-control select2" name="bulan" required>
                                             <option value="">--Pilih Bulan--</option>
-                                            <option value="1">Januari</option>
-                                            <option value="2">Februari</option>
-                                            <option value="3">Maret</option>
-                                            <option value="4">April</option>
-                                            <option value="5">Mei</option>
-                                            <option value="6">Juni</option>
-                                            <option value="7">Juli</option>
-                                            <option value="8">Agustus</option>
-                                            <option value="9">September</option>
+                                            <option value="01">Januari</option>
+                                            <option value="02">Februari</option>
+                                            <option value="03">Maret</option>
+                                            <option value="04">April</option>
+                                            <option value="05">Mei</option>
+                                            <option value="06">Juni</option>
+                                            <option value="07">Juli</option>
+                                            <option value="08">Agustus</option>
+                                            <option value="09">September</option>
                                             <option value="10">Oktober</option>
                                             <option value="11">November</option>
                                             <option value="12">Desember</option>
